223cc338e0959b87e2212223065806f1e7b99886
iLeilei <bsz9527@yeah.net>
2020-04-08 22:03:00 +0800

    testzhong

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 public  class Test{
-	public static void main(String[] arg){
-		System.out.print("hello wword");
+	public static void main(String[] args){
+		System.out.print("hello world");
 	}
 
 }
